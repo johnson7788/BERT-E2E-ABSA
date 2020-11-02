fd859323db2749d46456f5bf7660cd02a24df321
johnson7788 <linghuchongxajh@gmail.com>
2020-11-02 09:09:09 +0800

    2020110209

--- t1.py
+++
@@ -1,9 +0,0 @@
-import torch
-#一共有多少GPU
-print(torch.cuda.device_count())
-#当前GPU是哪个
-print(torch.cuda.current_device())
-#当前GPU的名字
-print(torch.cuda.get_device_name(torch.cuda.current_device()))
-# torch是否正在使用GPU
-print(torch.cuda.is_available())
